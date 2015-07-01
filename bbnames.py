4a52c54ea305d59c0db542280d7f05a59902c4f8
Danielle Hill <goal2code@gmail.com>
2015-07-01 18:36:26 -0400

    Cleaned up whitespace

--- bbnames.py
+++ bbnames.py
@@ -43,16 +43,18 @@ names['biblical'].fillna('false', inplace=True)
 
 #5. Create a pivot tables, rename columns, add column for % and calculate %
 
-######  Define functions  #####
-#Function to rename columns
-def rename_columns(table):
-	table.columns = ['false', 'true']
+	######  Define functions  #####
 
-#Function to add column and calculate %
-def add_percent(table):
-	table['total'] = table['false'] + table['true']
-	table['%'] = (table['true'] / (table['true']+table['false'])) * 100
+	#Function to rename columns
+	def rename_columns(table):
+		table.columns = ['false', 'true']
 
+	#Function to add column and calculate %
+	def add_percent(table):
+		table['total'] = table['false'] + table['true']
+		table['%'] = (table['true'] / (table['true']+table['false'])) * 100
+
+	######  End define functions  #####
 
 #5A Pivot table summarizing the names by year and state
 year_gender_summary = pd.pivot_table(names, values='births', index=['year', 'state', 'gender'],columns=['biblical'], aggfunc=np.sum)
@@ -63,10 +65,6 @@ add_percent(year_gender_summary)
 state_summary = pd.pivot_table(names, values='births', index='state', columns=['biblical'], aggfunc=np.sum)
 rename_columns(state_summary)
 add_percent(state_summary)
-
-
-
-
 
 #6. Store table and names as csv	for visualization with d3.js and/or Tableau
 #year_gender_summary.to_csv('bbnames_summary.csv')
