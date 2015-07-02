7ac57f3316c09bdf4adbbb5fadbb03b57a9d7986
Danielle Hill <goal2code@gmail.com>
2015-07-02 18:10:55 -0400

    Updated pivot tables to include margins; refined add_percent()function

--- bbnames.py
+++ bbnames.py
@@ -47,22 +47,21 @@ names['biblical'].fillna('false', inplace=True)
 
 #Function to rename columns
 def rename_columns(table):
-	table.columns = ['false', 'true']
+	table.columns = ['false', 'true', 'total']
 
 #Function to add column and calculate %
 def add_percent(table):
-	table['total'] = table['false'] + table['true']
 	table['%'] = (table['true'] / (table['true']+table['false'])) * 100
 
 ######  End define functions  #####
 
 #5A Pivot table summarizing the names by year and state
-year_gender_summary = pd.pivot_table(names, values='births', index=['year', 'state', 'gender'],columns=['biblical'], aggfunc=np.sum)
+year_gender_summary = pd.pivot_table(names, values='births', index=['year', 'state', 'gender'],columns=['biblical'], aggfunc=np.sum, margins=True)
 rename_columns(year_gender_summary)
 add_percent(year_gender_summary)
 
 #5B Pivot table calculating % biblical names by state for all years
-state_summary = pd.pivot_table(names, values='births', index='state', columns=['biblical'], aggfunc=np.sum)
+state_summary = pd.pivot_table(names, values='births', index='state', columns=['biblical'], aggfunc=np.sum, margins=True)
 rename_columns(state_summary)
 add_percent(state_summary)
 
@@ -70,3 +69,5 @@ add_percent(state_summary)
 #year_gender_summary.to_csv('bbnames_summary.csv')
 #state_summary.to_csv('state_summary.csv')
 #names.to_csv('bbnames_raw.csv')
+
+# pivot table for gender summary: table = pd.pivot_table(names, values='births', index=['state', 'gender'], columns='biblical', aggfunc=np.sum, margins=True)
