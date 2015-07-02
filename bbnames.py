a3fa11cbf3f20b19e90c1591242512c5a37ef55e
Danielle Hill <goal2code@gmail.com>
2015-07-02 17:01:20 -0400

    Changed indention

--- bbnames.py
+++ bbnames.py
@@ -43,18 +43,18 @@ names['biblical'].fillna('false', inplace=True)
 
 #5. Create a pivot tables, rename columns, add column for % and calculate %
 
-	######  Define functions  #####
+######  Define functions  #####
 
-	#Function to rename columns
-	def rename_columns(table):
-		table.columns = ['false', 'true']
+#Function to rename columns
+def rename_columns(table):
+	table.columns = ['false', 'true']
 
-	#Function to add column and calculate %
-	def add_percent(table):
-		table['total'] = table['false'] + table['true']
-		table['%'] = (table['true'] / (table['true']+table['false'])) * 100
+#Function to add column and calculate %
+def add_percent(table):
+	table['total'] = table['false'] + table['true']
+	table['%'] = (table['true'] / (table['true']+table['false'])) * 100
 
-	######  End define functions  #####
+######  End define functions  #####
 
 #5A Pivot table summarizing the names by year and state
 year_gender_summary = pd.pivot_table(names, values='births', index=['year', 'state', 'gender'],columns=['biblical'], aggfunc=np.sum)
